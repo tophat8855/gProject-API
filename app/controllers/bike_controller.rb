5fb12aa4afeee70c65c3418a417966d647327748
tophat8855 <hrfarley@gmail.com>
2015-03-04 15:00:09 -0800

    ready to start moving bike stuff over

---
+++ app/controllers/bike_controller.rb
@@ -0,0 +1,13 @@
+class BikeController < ApplicationController
+  def index
+
+    start = params["start"].gsub!(' ', "+")
+    ending = params["end"].gsub!(' ', '+') 
+
+    response = RestClient.get "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + start + "&destinations=" + ending +"&mode=walking&units=imperial&key=AIzaSyA6LTKJE21pYmBhAU0PiVndEyb7YZ9Cepo"
+    @results = JSON.parse(response.body)
+
+    @results = @results["rows"][0]["elements"][0]
+    render json: @results
+  end
+end
--- app/controllers/walk_controller.rb
+++ app/controllers/walk_controller.rb
@@ -1,8 +1,8 @@
 class WalkController < ApplicationController
   def index
 
-    start = params["start"].gsub!(' ', "+") #"1163+63rd+st+oakland+ca"
-    ending = params["end"].gsub!(' ', '+') #"1200+Park+Ave+Emeryville+CA"
+    start = params["start"].gsub!(' ', "+")
+    ending = params["end"].gsub!(' ', '+') 
 
     response = RestClient.get "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + start + "&destinations=" + ending +"&mode=walking&key="
     @results = JSON.parse(response.body)
