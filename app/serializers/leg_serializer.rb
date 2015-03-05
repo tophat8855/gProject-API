15e4516c7ad051a6ed046693ad128745eb843e2f
tophat8855 <hrfarley@gmail.com>
2015-03-05 13:49:06 -0800

    serializers for trip and legs

--- app/serializers/leg_serializer.rb
+++ app/serializers/leg_serializer.rb
@@ -1,3 +1,3 @@
 class LegSerializer < ActiveModel::Serializer
-  attributes :id, :type, :start_location, :end_location, :distance, :emissions, :description
+  attributes :id, :type, :start_location, :end_location, :distance, :emissions, :description, :trip_id
 end
