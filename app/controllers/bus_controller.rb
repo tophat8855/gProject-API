23b256e8f2b960f1f8ba7f8c4b3db2ef311ec929
tophat8855 <hrfarley@gmail.com>
2015-03-30 09:35:46 -0700

    JSON for bus routes officially works

--- app/controllers/bus_controller.rb
+++ app/controllers/bus_controller.rb
@@ -11,7 +11,7 @@ class BusController < ApplicationController
 
     sch_routeid = BusImporter.get_sch_routeid(@route)
     sch_patternid = BusImporter.get_sch_patternid(sch_routeid, @direction)
-    cpt_stoppointid = BusImporter.get_cpt_stoppoingid(sch_patternid)
+    cpt_stoppointid = BusImporter.get_cpt_stoppointid(sch_patternid)
     latlng_array = BusImporter.get_latlng_array(cpt_stoppointid)
 
     start_latlng = BusImporter.get_latlng(@start)
@@ -23,8 +23,10 @@ class BusController < ApplicationController
     start_seq_no = BusImporter.get_sequence_no(start_cpt_stoppointid, sch_patternid)
     end_seq_no = BusImporter.get_sequence_no(end_cpt_stoppointid, sch_patternid)
 
-    @distance = BusImporter.get_distance_of_leg(start_seq_no, end_seq_no, sch_patternid)
-    @emissions = 0.15873264 * @distance   #emissions data from http://www.buses.org/files/ComparativeEnergy.pdf
+    @distance = BusImporter.get_distance_of_leg(start_seq_no, end_seq_no, sch_patternid).round(2)
+    @emissions = (0.15873264 * @distance).round(2)   #emissions data from http://www.buses.org/files/ComparativeEnergy.pdf
+
+    @results = Hash.new()
 
     nested_array = []
 
@@ -33,6 +35,7 @@ class BusController < ApplicationController
     result["end"] = @ending
     result["route"] = @route
     result["direction"] = @direction
+    result["distance"] = @distance
     result["emissions"] = @emissions
 
     nested_array.push(result)
