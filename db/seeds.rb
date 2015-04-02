ddb596db148dce6105e2fade906920eff5dc1803
tophat8855 <hrfarley@gmail.com>
2015-04-02 10:45:22 -0700

    modifying seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,7 @@
 
 User.create([
   {email: 'pink@mail.com',
-   password: '12345678', password_confirmation: '12345678'}
+   password: '12345678', password_confirmation: '12345678'},
+  {email: 'green@mail.com',
+    password: '12345678', password_confirmation: '12345678'},
 ])
