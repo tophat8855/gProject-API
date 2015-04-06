4859b7b61d5073782c0e9bf471186f5648976a7c
tophat8855 <hrfarley@gmail.com>
2015-04-06 14:51:34 -0700

    ready for heroku maybe?

--- lib/bus/bus_importer.rb
+++ lib/bus/bus_importer.rb
@@ -1,12 +1,14 @@
 require 'csv'
 
 class BusImporter
-    @@route_csv = CSV.read('data/ROUTE.csv')
-    @@pattern_csv = CSV.read('data/PATTERN.csv')
-    @@patternstop_csv = CSV.read('data/PATTERNSTOP.csv')
-    @@stop_csv = CSV.read('data/STOP.csv')
+  def initialize(route_path, pattern_path, pattern_stop_path, stop_path)
+    @route_csv = CSV.read(route_path)
+    @pattern_csv = CSV.read(pattern_path)
+    @patternstop_csv = CSV.read(pattern_stop_path)
+    @stop_csv = CSV.read(stop_path)
+  end
 
-  def self.import
+  def import
     all_routes = get_sch_patternids_of_all
     all_stops = all_routes.map do |route|
       get_latlng_array(get_cpt_stoppointid(route))
@@ -47,9 +49,9 @@ class BusImporter
     end
   end
 
-  def self.get_sch_routeid(route)
+  def get_sch_routeid(route)
     sch_routeid = 0
-    @@route_csv.each do |row|
+    @route_csv.each do |row|
       if row[10] == route
         sch_routeid = row[0]
       end
@@ -57,9 +59,9 @@ class BusImporter
     sch_routeid
   end
 
-  def self.get_sch_patternid(sch_routeid, direction)
+  def get_sch_patternid(sch_routeid, direction)
     sch_patternid = 0
-    @@pattern_csv.each do |row|
+    @pattern_csv.each do |row|
       if (row[6] == sch_routeid) && (row[9].strip == direction)
         sch_patternid = row[0]
       end
@@ -67,9 +69,9 @@ class BusImporter
     sch_patternid
   end
 
-  def self.get_cpt_stoppointid(sch_patternid)
+  def get_cpt_stoppointid(sch_patternid)
     cpt_stoppointid = []
-    @@patternstop_csv.each do |row|
+    @patternstop_csv.each do |row|
       if row[5] == sch_patternid
         cpt_stoppointid << [row[1].to_i, row[7]]
       end
@@ -82,7 +84,7 @@ class BusImporter
     sorted_stoppoints
   end
 
-  def self.get_latlng(address)
+  def get_latlng(address)
     address = address.gsub!(' ', "+")
     response = RestClient.get "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + ENV['API_KEY']
     results = JSON.parse(response.body)
@@ -93,7 +95,7 @@ class BusImporter
     address_latlng = [lat, lng]
   end
 
-  def self.distance(point1, point2)
+  def distance(point1, point2)
     x2 = point2[0]
     x1 = point1[0]
     y2 = point2[1]
@@ -102,10 +104,10 @@ class BusImporter
     distance = Math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
   end
 
-  def self.get_latlng_array(cpt_stoppointid)
+  def get_latlng_array(cpt_stoppointid)
     latlng_array = []
 
-    @@stop_csv.each do |row|
+    @stop_csv.each do |row|
       if cpt_stoppointid.include? row[0]
         latlng_array << [row[8].to_f, row[6].to_f]
       end
@@ -113,7 +115,7 @@ class BusImporter
     latlng_array
   end
 
-  def self.desired_cpt_stoppointid(latlng_comp, latlng_array)
+  def desired_cpt_stoppointid(latlng_comp, latlng_array)
     desired_cpt_stoppointid = ''
 
     dist_array = latlng_array.map do |point|
@@ -124,7 +126,7 @@ class BusImporter
     closest_index = dist_array.index(closest)
     desired_latlng = latlng_array[closest_index]
 
-    @@stop_csv.each do |row|
+    @stop_csv.each do |row|
       float_lat = row[8].to_f
       float_lng = row[6].to_f
 
@@ -135,13 +137,13 @@ class BusImporter
     desired_cpt_stoppointid
   end
 
-  def self.my_trip_latlng(start_stopid, end_stopid, patternid)
+  def my_trip_latlng(start_stopid, end_stopid, patternid)
     trip_array = []
 
     start_sequence_no = get_sequence_no(start_stopid, patternid).to_i
     end_sequence_no = get_sequence_no(end_stopid, patternid).to_i
 
-    @@patternstop_csv.each do |row|
+    @patternstop_csv.each do |row|
       if (row[5] == patternid) && (start_sequence_no..end_sequence_no).include?(row[1].to_i)
         trip_array << [row[1].to_i, row[7]]
       end
@@ -153,10 +155,10 @@ class BusImporter
     get_latlng_array(cpt_list)
   end
 
-  def self.get_sequence_no(stopid, patternid)
+  def get_sequence_no(stopid, patternid)
     sequence_no = 0
 
-    @@patternstop_csv.each do |row|
+    @patternstop_csv.each do |row|
       if row[5] == patternid && row[7] == stopid
         sequence_no = row[1]
       end
@@ -165,10 +167,10 @@ class BusImporter
     sequence_no
   end
 
-  def self.get_sch_patternids_of_all
+  def get_sch_patternids_of_all
     all_routes = []
 
-    @@patternstop_csv.each do |row|
+    @patternstop_csv.each do |row|
       if !all_routes.include? row[5]
         all_routes << row[5]
       end
@@ -177,7 +179,7 @@ class BusImporter
     all_routes
   end
 
-  def self.get_distance_of_leg(start_seq_no, end_seq_no, pattern_id)
+  def get_distance_of_leg(start_seq_no, end_seq_no, pattern_id)
     distance_array = $redis.get(pattern_id)
     array = distance_array.gsub!(/[^0-9.,]/i, '').split(",").map!(&:to_f)
 
